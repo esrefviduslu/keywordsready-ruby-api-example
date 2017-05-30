acbc2de1c2c14c038530dcef7331a14c748cd614
esrefviduslu <esref.viduslu@lab2023.com>
2017-05-30 17:49:11 +0300

    Analyzes controller edited

--- app/controllers/analyzes_controller.rb
+++ app/controllers/analyzes_controller.rb
@@ -9,8 +9,8 @@ class AnalyzesController < ApplicationController
     if !verify_recaptcha
       redirect_to new_analyze_path
     else
-      hash['url'].present? ?     request = {'url': hash['url']}         : request = nil
-      hash['api_key'].present? ? headers = {'api-key': hash['api_key']} : headers = nil
+      request = {'url': hash['url']}
+      headers = {'api-key': hash['api_key']}
       response    = RestClient.post url, request, headers
       @response   = JSON.parse(response.body)
     end
